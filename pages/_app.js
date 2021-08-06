7c474582cfd3132c219ef769d480719631405d0c
Efe <48628713+Raader@users.noreply.github.com>
2021-08-06 16:36:58 +0300

    setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import 'tailwindcss/tailwind.css'
+import Head from "next/head";
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Realgar</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
