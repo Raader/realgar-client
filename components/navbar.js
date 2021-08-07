a8b7742f0dab69ce0717b14ae2aff78338efbe47
Efe <48628713+Raader@users.noreply.github.com>
2021-08-07 22:47:51 +0300

    fix navbar pad

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,7 @@
 const Navbar = ({ children }) => {
   return (
-    <nav className="border-b bg-white shadow-md sticky top-0">
-      <div className="container mx-auto flex flex-row flex-nowrap items-center py-4 px-2 md:px-10">
+    <nav className="border-b bg-white shadow-md sticky top-0 py-4 px-2 md:px-10">
+      <div className="container mx-auto flex flex-row flex-nowrap items-center md:px-4">
         {children}
       </div>
     </nav>
