f4dad54f7b5fad4cee4baf9c4bd8472975520609
Efe <48628713+Raader@users.noreply.github.com>
2021-08-07 14:30:27 +0300

    brand logos

--- components/button.js
+++ components/button.js
@@ -1,10 +1,12 @@
-const Button = ({ children, onClick, block }) => {
+const Button = ({ children, onClick, block, className }) => {
   return (
     <button
       onClick={onClick}
-      className={`appearance-none bg-gray-200 px-4 py-2 rounded-md hover:bg-gray-300 transition duration-200 border font-semibold ${
-        block ? "block w-full" : ""
-      }`}
+      className={
+        `appearance-none bg-gray-200 px-4 py-2 rounded-md hover:bg-gray-300 transition duration-200 border font-semibold ${
+          block ? "block w-full" : ""
+        } ` + className
+      }
     >
       {children}
     </button>
--- components/login_form.js
+++ components/login_form.js
@@ -2,6 +2,10 @@ import Button from "../components/button";
 import Form from "../components/form";
 import Input from "../components/input";
 import PrimaryButton from "../components/primary_button";
+import Image from "next/image";
+import GoogleIcon from "./google_icon";
+import GithubIcon from "./github_icon";
+import AppleIcon from "./apple_icon";
 
 const LoginForm = () => {
   return (
@@ -15,8 +19,24 @@ const LoginForm = () => {
       </div>
       <PrimaryButton block>Log Me In</PrimaryButton>
       <div className="w-full border-b text-gray-500 mb-2 mt-2">OR</div>
-      <Button block>Continue With Google</Button>
-      <Button block>Continue With Github</Button>
+      <Button block className="bg-blue-500 hover:bg-blue-600  text-white">
+        <div className="flex flex-row items-center gap-2">
+          <GoogleIcon className="w-5 h-5 inline-block"></GoogleIcon>
+          <div>Continue With Google</div>
+        </div>
+      </Button>
+      <Button block className="bg-black hover:bg-gray-700 text-white">
+        <div className="flex flex-row items-center gap-2">
+          <AppleIcon className="w-5 h-5 inline-block"></AppleIcon>
+          <div>Continue With Apple</div>
+        </div>
+      </Button>
+      <Button block className=" bg-purple-600 hover:bg-purple-700 text-white">
+        <div className="flex flex-row items-center gap-2 text-center">
+          <GithubIcon className="w-5 h-5 inline-block"></GithubIcon>
+          <div>Continue With Github</div>
+        </div>
+      </Button>
       <div className="text-gray-400 m-2">
         Don't have an account? <a className="text-green-300">Sign up here</a>
       </div>
