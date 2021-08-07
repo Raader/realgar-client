590140c35e144b6c062f242015c8d75b3102bea1
Efe <48628713+Raader@users.noreply.github.com>
2021-08-07 15:22:50 +0300

    navbar sticky

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,6 @@
 const Navbar = ({ children }) => {
   return (
-    <nav className="border-b bg-white shadow-sm">
+    <nav className="border-b bg-white shadow-sm sticky top-0">
       <div className="container mx-auto flex flex-row flex-nowrap items-center py-4 px-2 md:px-10">
         {children}
       </div>
