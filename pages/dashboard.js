933d62a68f28765a761bdd45477ba5247cb9565b
Efe <48628713+Raader@users.noreply.github.com>
2021-08-08 13:50:36 +0300

    default icon for dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,10 @@
-import { CashIcon } from "@heroicons/react/solid";
+import { CashIcon, PlusIcon } from "@heroicons/react/solid";
 import AppleMusicIcon from "../components/icons/apple_music_icon";
 import NetflixIcon from "../components/icons/netflix_icon";
 import SpotifyIcon from "../components/icons/spotify_icon";
 import Layout from "../components/layout";
 import Payment from "../components/payment";
+import Button from "../components/button";
 
 const icons = {
   appleMusic: (
@@ -19,6 +20,11 @@ const icons = {
   spotify: (
     <div className="bg-green-400 rounded-xl p-2">
       <SpotifyIcon className="w-6 h-6 text-white"></SpotifyIcon>
+    </div>
+  ),
+  default: (
+    <div className="bg-gray-300 rounded-xl p-2">
+      <CashIcon className="w-6 h-6 text-gray-100"></CashIcon>
     </div>
   ),
 };
@@ -59,13 +65,18 @@ const payments = [
 const Dashboard = () => {
   return (
     <Layout>
-      <h2 className="text-3xl font-semibold mb-4">Recurring Payments</h2>
+      <div className="pb-2 mb-4 border-b-2 text-gray-800 flex items-middle">
+        <h3 className="text-3xl font-semibold">Recurring Payments</h3>
+        <Button className="ml-auto">
+          <PlusIcon className="w-5 h-5"></PlusIcon>
+        </Button>
+      </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {payments.map((payment) => (
           <Payment
             {...payment}
             className=""
-            icon={icons[payment.icon]}
+            icon={icons[payment.icon] || icons.default}
           ></Payment>
         ))}
       </div>
