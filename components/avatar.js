546769c3d3247a18fd10bbb5418f5809dce7882e
Efe <48628713+Raader@users.noreply.github.com>
2021-08-09 17:16:52 +0300

    usermenu fixed height

--- components/avatar.js
+++ components/avatar.js
@@ -2,8 +2,8 @@ import { UserIcon } from "@heroicons/react/outline";
 
 const Avatar = () => {
   return (
-    <div className="inline-block p-1 bg-gray-200 rounded-full border text-gray-600">
-      <UserIcon className="w-5 h-5"></UserIcon>
+    <div className="p-1 bg-gray-200 rounded-full border text-gray-500 w-6 h-6 overflow-hidden">
+      <UserIcon className=""></UserIcon>
     </div>
   );
 };
--- components/user_menu.js
+++ components/user_menu.js
@@ -4,7 +4,7 @@ import NavLink from "./nav_link";
 
 const UserMenu = ({ user }) => {
   return (
-    <NavLink icon={<Avatar></Avatar>} className="hover:text-gray-800 text-xl">
+    <NavLink icon={<Avatar></Avatar>} className="hover:text-gray-800 p-0">
       <div className="flex items-center space-x-1">
         <div>{user?.username}</div>{" "}
         <ChevronDownIcon className="h-5 w-5"></ChevronDownIcon>
