0612628a0f964a27e5b4d387f29d6a80b584f265
Efe <48628713+Raader@users.noreply.github.com>
2021-08-10 20:52:55 +0300

    fix navbar start

--- components/navbar_start.js
+++ components/navbar_start.js
@@ -1,7 +1,5 @@
 const NavbarStart = ({ children }) => {
-  return (
-    <div className="lg:flex flex-row items-center ml-6 hidden">{children}</div>
-  );
+  return <div className="lg:flex flex-row items-center hidden">{children}</div>;
 };
 
 export default NavbarStart;
