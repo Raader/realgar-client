def801a88470d8221a0e7ee85a7425593a884f78
Efe <48628713+Raader@users.noreply.github.com>
2021-08-11 16:24:17 +0300

    clear button style changes

--- components/clear_button.js
+++ components/clear_button.js
@@ -4,7 +4,7 @@ const ClearButton = ({ className, children, ...props }) => {
   return (
     <Button
       className={
-        "bg-transparent hover:bg-gray-100 border-none block w-full text-left" +
+        "bg-transparent hover:bg-gray-100 border-none block w-full text-left text-gray-600 hover:text-gray-800" +
         " " +
         className
       }
