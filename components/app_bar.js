819b77ab4767fa165edf10c566658dcccf46ed1a
Efe <48628713+Raader@users.noreply.github.com>
2021-08-15 00:07:06 +0300

    highligh nav

--- components/app_bar.js
+++ components/app_bar.js
@@ -11,30 +11,61 @@ import {
 } from "@heroicons/react/solid";
 import MenuDropdown from "./menu_dropdown";
 import UserMenu from "./user_menu";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MenuButton from "./menu_btn";
 import NavbarDrop from "./navbar_drop";
+import { useRouter } from "next/dist/client/router";
 
 const NavLinks = () => {
+  const router = useRouter();
+  const [links, setLinks] = useState([
+    {
+      icon: <HomeIcon className="h-5 w-5"></HomeIcon>,
+      href: "/",
+      name: "Home",
+      selected: true,
+    },
+    {
+      icon: <InformationCircleIcon className="h-5 w-5"></InformationCircleIcon>,
+      href: "/about",
+      name: "About",
+      selected: false,
+    },
+    {
+      icon: <ViewBoardsIcon className="h-5 w-5"></ViewBoardsIcon>,
+      href: "/dashboard",
+      name: "Dashboard",
+      selected: false,
+    },
+  ]);
+
+  useEffect(() => {
+    setLinks((prev) =>
+      prev.map((link) =>
+        link.href === router.asPath
+          ? { ...link, selected: true }
+          : { ...link, selected: false }
+      )
+    );
+  }, [router.asPath]);
   return (
     <>
-      <NavLink icon={<HomeIcon className="h-5 w-5"></HomeIcon>} href="/">
-        Home
-      </NavLink>
-      <NavLink
-        icon={
-          <InformationCircleIcon className="h-5 w-5"></InformationCircleIcon>
-        }
-        href="/about"
-      >
-        About
-      </NavLink>
-      <NavLink
-        icon={<ViewBoardsIcon className="h-5 w-5"></ViewBoardsIcon>}
-        href="/dashboard"
-      >
-        Dashboard
-      </NavLink>
+      {links.map((link) =>
+        !link.selected ? (
+          <NavLink icon={link.icon} href={link.href} key={link.name}>
+            {link.name}
+          </NavLink>
+        ) : (
+          <NavLink
+            icon={link.icon}
+            href={link.href}
+            key={link.name}
+            className="bg-gray-400 text-green-500 bg-opacity-10"
+          >
+            {link.name}
+          </NavLink>
+        )
+      )}
     </>
   );
 };
