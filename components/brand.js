2eae250b1956c5b775e8dba127a41eb84ad0474c
Efe <48628713+Raader@users.noreply.github.com>
2021-08-17 17:53:58 +0300

    optimize brand image component
    used static import to enable nextjs image optimization
    
    set priority prop to true to prevent the side from loading without brand

--- components/brand.js
+++ components/brand.js
@@ -1,8 +1,15 @@
 import Image from "next/image";
+import logo from "../public/logo.svg";
 
 const Brand = ({ height = 40, width = 200 }) => {
   return (
-    <Image src="/logo.svg" height={height} width={width} alt="realgar"></Image>
+    <Image
+      src={logo}
+      height={height}
+      width={width}
+      alt="realgar"
+      priority={true}
+    ></Image>
   );
 };
 
