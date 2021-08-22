5edc412b9cf6919f001291bffcca477118c2073c
Efe <48628713+Raader@users.noreply.github.com>
2021-08-22 13:47:15 +0300

    Fix primary button

--- components/primary_button.js
+++ components/primary_button.js
@@ -1,13 +1,17 @@
 import Button from "./button";
 
-const PrimaryButton = ({ children, className, ...props }) => {
+const PrimaryButton = ({ children, className, block, ...props }) => {
   return (
-    <Button
-      className={"bg-green-400 hover:bg-green-500 text-white" + " " + className}
+    <button
+      className={
+        `appearance-none text-white bg-green-400 px-4 py-2 rounded-md hover:bg-green-500 transition duration-200 font-semibold select-none${
+          block ? "block w-full" : ""
+        } ` + className
+      }
       {...props}
     >
       {children}
-    </Button>
+    </button>
   );
 };
 
