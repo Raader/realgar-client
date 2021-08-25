fb7a952b1ca588cbda31add3e2d397b854cde649
Efe <48628713+Raader@users.noreply.github.com>
2021-08-25 17:45:14 +0300

    Add disabled state to input component

--- components/common/input.js
+++ components/common/input.js
@@ -1,12 +1,13 @@
-const Input = ({ placeholder, type, id, value, onChange }) => {
+const Input = ({ placeholder, type, id, value, onChange, disabled }) => {
   return (
     <input
       id={id}
-      className="appearance-none rounded-md p-2 block w-full border-2 border-gray-300 focus:border-green-400 outline-none transition duration-200"
+      className="appearance-none rounded-md p-2 block w-full border-2 border-gray-300 focus:border-green-400 outline-none transition duration-200 disabled:opacity-50"
       placeholder={placeholder}
       type={type}
       value={value}
       onChange={onChange}
+      disabled={disabled}
     ></input>
   );
 };
