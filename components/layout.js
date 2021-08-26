439a3ab4cad3568a65e7835f0180728f4e0be6a6
Efe <48628713+Raader@users.noreply.github.com>
2021-08-26 18:42:23 +0300

    Add layout animation

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,9 @@ const Layout = ({ children }) => {
   return (
     <div className="min-h-screen font-sans bg-gray-50">
       <AppBar></AppBar>
-      <div className="container mx-auto p-4">{children}</div>
+      <div className="container mx-auto p-4 w-full overflow-x-hidden">
+        <div className="layout-animation">{children}</div>
+      </div>
       <Footer></Footer>
     </div>
   );
