a1cccc0c06bc943f3f08b4c266768b86fd3c8c69
Efe <48628713+Raader@users.noreply.github.com>
2021-08-29 14:42:35 +0300

    Adjust colors of input and label elements
    to ensure visual hierarchy

--- components/common/input.js
+++ components/common/input.js
@@ -2,7 +2,7 @@ const Input = ({ placeholder, type, id, value, onChange, disabled, error }) => {
   return (
     <input
       id={id}
-      className={`appearance-none rounded-md p-2 block w-full border-2 border-gray-300 focus:border-green-400 outline-none transition duration-200 disabled:opacity-50 ${
+      className={`appearance-none rounded-md p-2 block w-full border-2 border-gray-300 focus:border-green-400 outline-none transition duration-200 disabled:opacity-50 text-gray-600 ${
         error ? "border-red-400 focus:border-red-400" : ""
       }`}
       placeholder={placeholder}
