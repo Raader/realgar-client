cd6fb5f0d2532988b237cbb42643ef8943789d7c
Efe <48628713+Raader@users.noreply.github.com>
2021-09-03 23:47:57 +0300

    Update navbar style

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,6 @@
 const Navbar = ({ children }) => {
   return (
-    <nav className="bg-white py-1 px-4 md:py-4">
+    <nav className="bg-white py-1 px-4 md:py-4 drop-shadow lg:drop-shadow-none bg-opacity-90 backdrop-blur">
       <div className="container mx-auto flex flex-row flex-nowrap items-center md:px-4">
         {children}
       </div>
