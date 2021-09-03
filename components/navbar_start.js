baac8771108c3604105be4fcc618061ca48cd0be
Efe <48628713+Raader@users.noreply.github.com>
2021-09-03 13:23:35 +0300

    Add margin to nav start

--- components/navbar_start.js
+++ components/navbar_start.js
@@ -1,5 +1,7 @@
 const NavbarStart = ({ children }) => {
-  return <div className="lg:flex flex-row items-center hidden">{children}</div>;
+  return (
+    <div className="lg:flex flex-row items-center hidden ml-8">{children}</div>
+  );
 };
 
 export default NavbarStart;
