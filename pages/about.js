12480b25bdd41141c307aa9404a6f27481617f74
Efe <48628713+Raader@users.noreply.github.com>
2021-09-03 13:03:32 +0300

    Create an about page

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (<Layout>
+    <div className="prose">
+      <h3>About</h3>
+      <p>Realgar is a free and open source subscription tracker. It helps with keeping track of all subscriptions you might have.</p>
+      <p>More and more companies started to use subscription models and there for it gets harder to keep track especially when you are using their free trials.</p>
+    </div>
+  </Layout>);
+}
+ 
+export default AboutPage;
