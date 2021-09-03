624e9db314d6d44adf2cdf32d90e6ad425f220c9
Efe <48628713+Raader@users.noreply.github.com>
2021-09-03 13:22:29 +0300

    Add classname prop to icon text

--- components/common/icon_text.js
+++ components/common/icon_text.js
@@ -1,8 +1,8 @@
-const IconText = ({ icon, children }) => {
+const IconText = ({ icon, children, className }) => {
   return (
     <div className="flex flex-row items-center">
       {icon}
-      <div className="ml-1">{children}</div>
+      <div className={`ml-1 ${className}`}>{children}</div>
     </div>
   );
 };
