18a7c8bfe77ca7a9b6be970fa9998d31dbecf577
Efe <48628713+Raader@users.noreply.github.com>
2021-09-04 10:24:15 +0300

    Add drop shadow property to navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,10 @@
-const Navbar = ({ children }) => {
+const Navbar = ({ children, dropShadow = true }) => {
   return (
-    <nav className="bg-white py-1 px-4 md:py-4 drop-shadow lg:drop-shadow-none bg-opacity-90 backdrop-blur">
+    <nav
+      className={`bg-white py-1 px-4 md:py-4 drop-shadow ${
+        dropShadow ? "lg:drop-shadow" : "lg:drop-shadow-none"
+      } bg-opacity-90 backdrop-blur`}
+    >
       <div className="container mx-auto flex flex-row flex-nowrap items-center md:px-4">
         {children}
       </div>
